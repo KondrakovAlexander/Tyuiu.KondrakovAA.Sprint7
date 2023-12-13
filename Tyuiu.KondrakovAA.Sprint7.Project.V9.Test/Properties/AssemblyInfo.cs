93580538ca27335aaf90abef379d743a4ff7acee
Tyuiu.KondrakovAA@outlook.com <Tyuiu.KondrakovAA@outlook.com>
2023-12-14 00:12:19 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.KondrakovAA.Sprint7.Project.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KondrakovAA.Sprint7.Project.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KondrakovAA.Sprint7.Project.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7e063ddc-a49a-4945-b63c-bce6c97c2013")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
